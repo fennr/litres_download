dbe383e11efd8eeb43224a93b5c0e53e730aff13
relyay <relya@tuta.io>
2026-08-06 20:12:38 +0300

    fpdf -> Pillow

--- create_pdf.py
+++ create_pdf.py
@@ -1,22 +1,30 @@
-from fpdf import FPDF
 import os
+from PIL import Image
 from config import BOOK_NAME, BOOK_ID
 
-pdf = FPDF()
-folder = os.listdir(f"books/{BOOK_NAME}_{BOOK_ID}")
-folder = sorted(folder, key=lambda x: int(os.path.splitext(x)[0]))
-images = []
+path_to_folder = f"books/{BOOK_NAME}_{BOOK_ID}"
+folder = os.listdir(path_to_folder)
 
+folder = sorted(folder, key=lambda x: int(os.path.splitext(x)[0]))
+
+images = []
 for file in folder:
-    images.append(f"books/{BOOK_NAME}_{BOOK_ID}/{file}")
+    images.append(f"{path_to_folder}/{file}")
 
 print(images)
 
-for image in images:
-    pdf.add_page()
-    pdf.image(image, 0, 0, 210, 297)
-    print(f"add page {image}")
+f_image = Image.open(images[0]).convert('RGB')
+print(f"add page {images[0]}")
+
+o_images = []
+for image_path in images[1:]:
+    img = Image.open(image_path).convert('RGB')
+    o_images.append(img)
+    print(f"add page {image_path}")
+
 print("create pdf...")
-pdf.output(f"books/{BOOK_NAME}_{BOOK_ID}.pdf", "F")
+
+output_path = f"books/{BOOK_NAME}_{BOOK_ID}.pdf"
+f_image.save(output_path, save_all=True, append_images=o_images)
 
 print("finish")
